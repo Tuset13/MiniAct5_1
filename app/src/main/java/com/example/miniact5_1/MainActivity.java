dd8f5b25e69084ca4faec8b824c203157a8371e7
Tuset13 <37141132+Tuset13@users.noreply.github.com>
2020-03-11 18:53:17 +0100

    Initial commit, part1.

--- app/src/main/java/com/example/miniact5_1/MainActivity.java
+++ app/src/main/java/com/example/miniact5_1/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.miniact5_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView twInfo = findViewById(R.id.textView);
+        TextView twState = findViewById(R.id.textView2);
+
+        ConnectivityManager cm =
+                (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
     }
 }
