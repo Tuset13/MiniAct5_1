93f83bc5857ddeeb89001b3ad3677a4be525a623
Tuset13 <37141132+Tuset13@users.noreply.github.com>
2020-03-12 18:53:49 +0100

    Inici de treballs en BackGround.

--- app/src/main/java/com/example/miniact5_1/MainActivity.java
+++ app/src/main/java/com/example/miniact5_1/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.miniact5_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.TextView;
 
+
+
 public class MainActivity extends AppCompatActivity {
+
+    ConnectivityManager cm;
+    NetworkInfo activeNetwork;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +25,11 @@ public class MainActivity extends AppCompatActivity {
         TextView twInfo = findViewById(R.id.textView);
         TextView twState = findViewById(R.id.textView2);
 
-        ConnectivityManager cm =
-                (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        cm =(ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        if(cm != null)
+        new CheckConnectivityTask().execute();
+
+        /*if(cm != null)
         {
             NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
 
@@ -40,6 +48,37 @@ public class MainActivity extends AppCompatActivity {
                 }
             } else {
                 twState.setText(R.string.noCon);
+            }
+        }*/
+    }
+
+    private class CheckConnectivityTask extends AsyncTask<Activity, Void, String>{
+
+        @Override
+        protected String doInBackground(Activity... activity) {
+            TextView twInfo = findViewById(R.id.textView);
+            TextView twState = findViewById(R.id.textView2);
+
+            if(cm != null)
+            {
+                activeNetwork = cm.getActiveNetworkInfo();
+
+                boolean isConnected = activeNetwork != null &&
+                        activeNetwork.isConnectedOrConnecting();
+                if(isConnected)
+                {
+                    twInfo.setText(activeNetwork.toString());
+                    if(activeNetwork.getType() == ConnectivityManager.TYPE_WIFI)
+                    {
+                        return (String) getText(R.string.wifiCon);
+                    }
+                    else if(activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE)
+                    {
+                        return (String) getText(R.string.mobCon);
+                    }
+                } else {
+                    return (String) getText(R.string.noCon);
+                }
             }
         }
     }
